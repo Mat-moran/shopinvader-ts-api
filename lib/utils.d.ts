9acbde7b67a8456a8d257d459dbac37e9d1f203b
Amadeo Morán <amadeo.moran@gmail.com>
2023-04-17 22:38:35 +0200

    clean all

--- lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { ZodSchema } from "zod";
-import { IApiResponse } from "./shopinvader-boundary";
-export declare const zodParse: <K>(schema: ZodSchema, fetchResult: any) => Promise<IApiResponse<K>>;
-//# sourceMappingURL=utils.d.ts.map
